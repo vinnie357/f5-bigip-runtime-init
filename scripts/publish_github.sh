f6ae6be95fc57c6cc8102c79a368e432770579c4
Andrey KASHCHEEV <a.kashcheev@f5.com>
2020-09-04 12:46:04 -0700

    update publish

--- scripts/publish_github.sh
+++ scripts/publish_github.sh
@@ -12,6 +12,8 @@ fi
 GIT_TAG=$1
 echo "RELEASE TAG: $GIT_TAG"
 
+
+######################
 echo "Configuring SSH"
 eval $(ssh-agent -s)
 test "$GIT_SSH_USER_PRIVATE_KEY" && (echo "$GIT_SSH_USER_PRIVATE_KEY" | tr -d '\r' | ssh-add -)
@@ -21,27 +23,26 @@ echo "$GIT_SSH_USER_PUBLIC_KEY" >> ~/.ssh/id_rsa.pub
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 git config user.name $GITLAB_USER_LOGIN
 git config user.email $GITLAB_USER_EMAIL
+######################
 
-echo "Cloning projects"
-# Create temp dir
-mkdir -p ./release/github/ && cd ./release/github/
-git clone git@github.com:f5devcentral/f5-bigip-runtime-init.git
-echo 'Completed cloning github repo'
-cd ./f5-bigip-runtime-init
-find . ! -name .git -delete
-cd ../../../
-cp -R ./src ./release/github/f5-bigip-runtime-init/
-cp -R ./examples ./release/github/f5-bigip-runtime-init/
-cp -R ./diagrams ./release/github/f5-bigip-runtime-init/
-cp .gitignore ./release/github/f5-bigip-runtime-init/
-cp package.json ./release/github/f5-bigip-runtime-init/
-cp package-lock.json ./release/github/f5-bigip-runtime-init/
-cp README.md ./release/github/f5-bigip-runtime-init/
-cp tsconfig.json ./release/github/f5-bigip-runtime-init/
-cd ./release/github/f5-bigip-runtime-init/
-pwd
-ls -la
+ALLOWED_DIRS=(src examples diagrams)
+ALLOWED_FILES=(.gitignore .gitallowed package.json package-lock.json README.md tsconfig.json)
+
+echo "Setting git origin"
 git remote rm origin && git remote add origin git@github.com:f5devcentral/f5-bigip-runtime-init.git
+echo "Removing everything from git"
+git rm -rf .
+
+echo "Adding allowed directories"
+for dir in "${ALLOWED_DIRS[@]}"; do
+    git checkout HEAD dir
+done
+
+echo "Adding allowed files"
+for file in "${ALLOWED_FILES[@]}"; do
+    git checkout HEAD file
+done
+
 git status
 git add -f .
 git commit -m "Release commited to $CI_COMMIT_REF_NAME tag" || echo "No changes, nothing to commit!"
